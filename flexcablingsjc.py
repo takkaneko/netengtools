ecb77b64567bad4c55538e0b590acd4d237182d8
Takumi Kaneko <takumi.kaneko@gmail.com>
2016-02-19 20:31:14 -0500

    sjc is now fully modularized with functions

--- flexcablingsjc.py
+++ flexcablingsjc.py
@@ -10,6 +10,8 @@ from flexresources import determine_trace_SJC,identify_switches,identify_switch_
 from flexresources import get_cabling_type,identify_cabling_attributes,typeDictionary
 from flexresources import summaryView,switch_pair,check_swport_usage,choose_sw1_or_sw2
 from flexresources import choose_switchport,choose_flexport_SJC,print_results_SJC
+from flexresources import ask_symmetric_or_not,choose_switchports_HA,choose_flexports_SJC_HA
+from flexresources import print_results_SJC_HA
 
 def main():
     # Prompt for username, password, and alloccode-depth (alloc)
@@ -76,145 +78,21 @@ def main():
     # Case II: Dual cabling type (hbeat, vmotion, pri+, sec+, or otherha).
     
     else:
-        while True:
-            try:
-                symmetric = input('Symmetric cabling (switchports)? [Y/n]: ').strip().lower()[0]
-                if not symmetric in ['y','n']:
-                    print('ERROR: DATA INVALID\n')
-                else:
-                    break
-            except (ValueError,IndexError):
-                print('ERROR: DATA INVALID\n')
-        while True:
-            try:
-                port1 = input('Choose the '+SW1+' module 4 SWITCHPORT number to use this time (1 - 48): ').strip()
-                if not int(port1) in [i for i in range(1,49)]:
-                    print('ERROR: DATA INVALID\n')
-                else:
-                    break
-            except (ValueError,IndexError):
-                print('ERROR: DATA INVALID\n')
-        if symmetric == 'n':
-            while True:
-                try:
-                    port2 = input('Choose the '+SW2+' port number: ').strip()
-                    if not int(port2) in [i for i in range(1,49)]:
-                        print('ERROR: DATA INVALID\n')
-                    else:
-                        break
-                except (ValueError,IndexError):
-                    print('ERROR: DATA INVALID\n')
-        else:
-            port2 = port1
-        while True:
-            try:
-                flexport1 = input('Select an available U43 FLX1 port (1 - 12) in the server rack: ').strip()
-                if not int(flexport1) in [i for i in range(1,13)]:
-                    print('ERROR: DATA INVALID\n')
-                else:
-                    break
-            except (ValueError,IndexError):
-                print('ERROR: DATA INVALID\n')
-        while True:
-            try:
-                ask_symmetric_flex = input('Use the symmetric FLX2 port '+str(int(flexport1)+24)+'? [Y/n]: ').strip().lower()
-                if not ask_symmetric_flex in ['y','n']:
-                    print('ERROR: DATA INVALID\n')
-                else:
-                    break
-            except (ValueError,IndexError):
-                print('ERROR: DATA INVALID\n')
-        if ask_symmetric_flex == 'y':
-            flexport2 = str(int(flexport1)+24)
-        else:
-            while True:
-                try:
-                    flexport2 = input('Select an available U43 FLX2 port (25 - 36) in the server rack: ').strip()
-                    if not int(flexport2) in [i for i in range(25,37)]:
-                        print('ERROR: DATA INVALID\n')
-                    else:
-                        break
-                except (ValueError,IndexError):
-                    print('ERROR: DATA INVALID\n')
-        prim_flexport = flexport1 if trace == 'straight' else flexport2
-        sec_flexport = flexport2 if trace == 'straight' else flexport1
-        prim_idf_num = '2' if trace == 'straight' else '23'
-        sec_idf_num = '23' if trace == 'straight' else '2'
-        if loc.rack in ['4','9','13','17']:
-            idf_port = flexport1
-        elif loc.rack in ['5','10','14','18']:
-            idf_port = str(int(flexport1) + 12)
-        elif loc.rack in ['6','11','15','20']:
-            idf_port = str(int(flexport1) + 24)
-        else: # if loc.rack in ['8','12','16','21']:
-            idf_port = str(int(flexport1) + 36)
-        prim_sw = priSW if pri_or_sec == 'pri' else secSW
-        sec_sw = pr2SW if pri_or_sec == 'pri' else se2SW
-        prim_port = port1 if trace == 'straight' else port2
-        sec_port = port2 if trace == 'straight' else port1
-        print()
-        print('*******************')
-        print('CABLING INSTUCTIONS')
-        print('*******************')
-        print()
-        print(sid+' ('+loc+')')
-        print()
-        print(typeDict[type][0]+':')
-        print()
-        print('Speed/Dup: '+spd+'/Full')
-        print()
-        print(' '+typeDict[type][0]+' prim.interface ->')
-        print('  PURPLE STRAIGHT -> U43 purple panel port '+prim_flexport+' ->')
-        print('  PURPLE STRAIGHT -> rack '+loc.row+'-'+prim_idf_num)
-        print('  PURPLE STRAIGHT -> purple panel U'+u_num+' p'+idf_port+' ->')
-        print('  PURPLE STRAIGHT -> '+prim_sw+' gi4/'+prim_port)
-        print()
-        print(' '+typeDict[type][0]+' sec.interface ->')
-        print('  PURPLE STRAIGHT -> U43 purple panel port '+sec_flexport+' ->')
-        print('  PURPLE STRAIGHT -> rack '+loc.row+'-'+sec_idf_num)
-        print('  PURPLE STRAIGHT -> purple panel U'+u_num+' p'+idf_port+' ->')
-        print('  PURPLE STRAIGHT -> '+sec_sw+' gi4/'+sec_port)
-        print()
-        print('*************************')
-        print('SWITCHPORT CONFIGURATIONS')
-        print('*************************')
-        print()
-        print(prim_sw+':')
-        print()
-        print('interface GigabitEthernet4/'+prim_port)
-        print(' description '+loc.row+'-'+loc.rack+'-'+loc.slot+'-'+typeDict[type][1]+' '+alloc+' server - '+sid)
-        print(' switchport')
-        print(' switchport access vlan '+str(vlan))
-        if pri_or_sec == 'pri':
-            print(' switchport mode access')
-        else:
-            print(' private-vlan host-association 11 '+str(vlan))
-            print(' switchport mode private-vlan host')
-        print(' load-interval 30')
-        print(' speed '+speed)
-        print(' '+duplex)
-        print(' spanning-tree portfast edge')
-        print(' no shut')
-        print('end')
-        print()
-        print(sec_sw+':')
-        print()
-        print('interface GigabitEthernet4/'+sec_port)
-        print(' description '+loc.row+'-'+loc.rack+'-'+loc.slot+'-'+typeDict[type][2]+' '+alloc+' server - '+sid)
-        print(' switchport')
-        print(' switchport access vlan '+str(vlan))
-        if pri_or_sec == 'pri':
-            print(' switchport mode access')
-        else:
-            print(' private-vlan host-association 11 '+str(vlan))
-            print(' switchport mode private-vlan host')
-        print(' load-interval 30')
-        print(' speed '+speed)
-        print(' '+duplex)
-        print(' spanning-tree portfast edge')
-        print(' no shut')
-        print('end')
-        print()
-
+        symmetric = ask_symmetric_or_not()
+        
+        [port1,port2] = choose_switchports_HA(SW1,SW2,symmetric)
+        
+        [prim_flexport,sec_flexport,
+         idf_port,prim_idf_num,sec_idf_num,
+         prim_sw,sec_sw,
+         prim_port,sec_port] = choose_flexports_SJC_HA(trace,loc,pri_or_sec,
+                                                       priSW,pr2SW,secSW,
+                                                       se2SW,port1,port2)
+        
+        print_results_SJC_HA(sid,loc,typeDict,type,spd,
+                             prim_flexport,prim_idf_num,u_num,
+                             idf_port,prim_sw,prim_port,
+                             sec_flexport,sec_idf_num,sec_sw,sec_port,
+                             alloc,vlan,pri_or_sec,speed,duplex)
 if __name__ == '__main__':
     main()
--- flexresources.py
+++ flexresources.py
@@ -324,6 +324,89 @@ def choose_switchport(use_this_switch):
             print('ERROR: DATA INVALID\n')
     return port
 
+def ask_symmetric_or_not():
+    while True:
+        try:
+            symmetric = input('Symmetric cabling (switchports)? [Y/n]: ').strip().lower()[0]
+            if not symmetric in ['y','n']:
+                print('ERROR: DATA INVALID\n')
+            else:
+                break
+        except (ValueError,IndexError):
+            print('ERROR: DATA INVALID\n')
+    return symmetric
+
+def choose_switchports_HA(SW1,SW2,symmetric):
+    while True:
+        try:
+            port1 = input('Choose the '+SW1+' module 4 SWITCHPORT number to use this time (1 - 48): ').strip()
+            if not int(port1) in [i for i in range(1,49)]:
+                print('ERROR: DATA INVALID\n')
+            else:
+                break
+        except (ValueError,IndexError):
+            print('ERROR: DATA INVALID\n')
+    if symmetric == 'n':
+        while True:
+            try:
+                port2 = input('Choose the '+SW2+' port number: ').strip()
+                if not int(port2) in [i for i in range(1,49)]:
+                    print('ERROR: DATA INVALID\n')
+                else:
+                    break
+            except (ValueError,IndexError):
+                print('ERROR: DATA INVALID\n')
+    else:
+        port2 = port1
+    return [port1,port2]
+
+
+###########################################
+# FUNCTIONS THAT ARE INTENDED ONLY FOR SJC:
+###########################################
+
+def getLOC_SJC():
+    # Prompts for a location and returns it as a Loccode object that is valid for SJC Boreas.
+    while True:
+        try:
+            loc = input('Enter a location code: ').strip().lower()
+            loc = Loccode(loc)
+            if not int(loc.rack) in [4,5,6,8,9,10,11,12,13,14,15,16,17,18,20,21]:
+                print("ERROR: INVALID LOCATION\n")
+            elif not 1<=int(loc.slot)<=12:
+                print("ERROR: INVALID LOCATION\n")
+            elif not loc.sr in ['sjc.c10','sjc.c4','sjc.c9']:
+                print("ERROR: INVALID LOCATION\n")
+            elif not int(loc.row) in [1,2,3,4]:
+                print("ERROR: INVALID LOCATION\n")
+            else:
+                break
+        except AttributeError:
+            print("ERROR: INVALID LOCATION\n")
+    return loc
+
+def idf_U_number_SJC(loc):
+    # Returns the U number (as a string) of the patch panel in the IDF racks.
+    if int(loc.rack) in [4,5,6,8]:
+        u_num = '5'
+    elif int(loc.rack) in [9,10,11,12]:
+        u_num = '4'
+    elif int(loc.rack) in [13,14,15,16]:
+        u_num = '3'
+    else: # int(loc.rack) in [17,18,20,21]
+        u_num = '2'
+    return u_num
+
+def determine_trace_SJC(loc):
+    # straight or reversed
+    sjc_straight_racks = [4,6,9,11,13,15,17,20]
+    sjc_reverse_racks = [5,8,10,12,14,16,18,21]
+    if int(loc.rack) in sjc_straight_racks:
+        trace = 'straight'
+    else:
+        trace = 'reversed'
+    return trace
+
 def choose_flexport_SJC(use_this_switch,SW1,loc):
     # non-HA
     flexpt_range = '1 - 12' if use_this_switch == SW1 else '25 - 36'
@@ -392,56 +475,123 @@ def print_results_SJC(sid,loc,spd,type,typeDict,flexport,idf_num,u_num,idf_port,
     print('end')
     print()
 
-
-###########################################
-# FUNCTIONS THAT ARE INTENDED ONLY FOR SJC:
-###########################################
-
-def getLOC_SJC():
-    # Prompts for a location and returns it as a Loccode object that is valid for SJC Boreas.
+def choose_flexports_SJC_HA(trace,loc,pri_or_sec,priSW,pr2SW,secSW,se2SW,port1,port2):
     while True:
         try:
-            loc = input('Enter a location code: ').strip().lower()
-            loc = Loccode(loc)
-            if not int(loc.rack) in [4,5,6,8,9,10,11,12,13,14,15,16,17,18,20,21]:
-                print("ERROR: INVALID LOCATION\n")
-            elif not 1<=int(loc.slot)<=12:
-                print("ERROR: INVALID LOCATION\n")
-            elif not loc.sr in ['sjc.c10','sjc.c4','sjc.c9']:
-                print("ERROR: INVALID LOCATION\n")
-            elif not int(loc.row) in [1,2,3,4]:
-                print("ERROR: INVALID LOCATION\n")
+            flexport1 = input('Select an available U43 FLX1 port (1 - 12) in the server rack: ').strip()
+            if not int(flexport1) in [i for i in range(1,13)]:
+                print('ERROR: DATA INVALID\n')
             else:
                 break
-        except AttributeError:
-            print("ERROR: INVALID LOCATION\n")
-    return loc
-
-def idf_U_number_SJC(loc):
-    # Returns the U number (as a string) of the patch panel in the IDF racks.
-    if int(loc.rack) in [4,5,6,8]:
-        u_num = '5'
-    elif int(loc.rack) in [9,10,11,12]:
-        u_num = '4'
-    elif int(loc.rack) in [13,14,15,16]:
-        u_num = '3'
-    else: # int(loc.rack) in [17,18,20,21]
-        u_num = '2'
-    return u_num
-
-def determine_trace_SJC(loc):
-    # straight or reversed
-    sjc_straight_racks = [4,6,9,11,13,15,17,20]
-    sjc_reverse_racks = [5,8,10,12,14,16,18,21]
-    if int(loc.rack) in sjc_straight_racks:
-        trace = 'straight'
+        except (ValueError,IndexError):
+            print('ERROR: DATA INVALID\n')
+    while True:
+        try:
+            ask_symmetric_flex = input('Use the symmetric FLX2 port '+str(int(flexport1)+24)+'? [Y/n]: ').strip().lower()
+            if not ask_symmetric_flex in ['y','n']:
+                print('ERROR: DATA INVALID\n')
+            else:
+                break
+        except (ValueError,IndexError):
+            print('ERROR: DATA INVALID\n')
+    if ask_symmetric_flex == 'y':
+        flexport2 = str(int(flexport1)+24)
     else:
-        trace = 'reversed'
-    return trace
+        while True:
+            try:
+                flexport2 = input('Select an available U43 FLX2 port (25 - 36) in the server rack: ').strip()
+                if not int(flexport2) in [i for i in range(25,37)]:
+                    print('ERROR: DATA INVALID\n')
+                else:
+                    break
+            except (ValueError,IndexError):
+                print('ERROR: DATA INVALID\n')
+    prim_flexport = flexport1 if trace == 'straight' else flexport2
+    sec_flexport = flexport2 if trace == 'straight' else flexport1
+    prim_idf_num = '2' if trace == 'straight' else '23'
+    sec_idf_num = '23' if trace == 'straight' else '2'
+    if loc.rack in ['4','9','13','17']:
+        idf_port = flexport1
+    elif loc.rack in ['5','10','14','18']:
+        idf_port = str(int(flexport1) + 12)
+    elif loc.rack in ['6','11','15','20']:
+        idf_port = str(int(flexport1) + 24)
+    else: # if loc.rack in ['8','12','16','21']:
+        idf_port = str(int(flexport1) + 36)
+    prim_sw = priSW if pri_or_sec == 'pri' else secSW
+    sec_sw = pr2SW if pri_or_sec == 'pri' else se2SW
+    prim_port = port1 if trace == 'straight' else port2
+    sec_port = port2 if trace == 'straight' else port1
+    return [prim_flexport,sec_flexport,idf_port,prim_idf_num,sec_idf_num,prim_sw,sec_sw,prim_port,sec_port]
 
-
-
-
+def print_results_SJC_HA(sid,loc,typeDict,type,spd,
+                         prim_flexport,prim_idf_num,u_num,
+                         idf_port,prim_sw,prim_port,
+                         sec_flexport,sec_idf_num,sec_sw,sec_port,
+                         alloc,vlan,pri_or_sec,speed,duplex):
+    print()
+    print('*******************')
+    print('CABLING INSTUCTIONS')
+    print('*******************')
+    print()
+    print(sid+' ('+loc+')')
+    print()
+    print(typeDict[type][0]+':')
+    print()
+    print('Speed/Dup: '+spd+'/Full')
+    print()
+    print(' '+typeDict[type][0]+' prim.interface ->')
+    print('  PURPLE STRAIGHT -> U43 purple panel port '+prim_flexport+' ->')
+    print('  PURPLE STRAIGHT -> rack '+loc.row+'-'+prim_idf_num)
+    print('  PURPLE STRAIGHT -> purple panel U'+u_num+' p'+idf_port+' ->')
+    print('  PURPLE STRAIGHT -> '+prim_sw+' gi4/'+prim_port)
+    print()
+    print(' '+typeDict[type][0]+' sec.interface ->')
+    print('  PURPLE STRAIGHT -> U43 purple panel port '+sec_flexport+' ->')
+    print('  PURPLE STRAIGHT -> rack '+loc.row+'-'+sec_idf_num)
+    print('  PURPLE STRAIGHT -> purple panel U'+u_num+' p'+idf_port+' ->')
+    print('  PURPLE STRAIGHT -> '+sec_sw+' gi4/'+sec_port)
+    print()
+    print('*************************')
+    print('SWITCHPORT CONFIGURATIONS')
+    print('*************************')
+    print()
+    print(prim_sw+':')
+    print()
+    print('interface GigabitEthernet4/'+prim_port)
+    print(' description '+loc.row+'-'+loc.rack+'-'+loc.slot+'-'+typeDict[type][1]+' '+alloc+' server - '+sid)
+    print(' switchport')
+    print(' switchport access vlan '+str(vlan))
+    if pri_or_sec == 'pri':
+        print(' switchport mode access')
+    else:
+        print(' private-vlan host-association 11 '+str(vlan))
+        print(' switchport mode private-vlan host')
+    print(' load-interval 30')
+    print(' speed '+speed)
+    print(' '+duplex)
+    print(' spanning-tree portfast edge')
+    print(' no shut')
+    print('end')
+    print()
+    print(sec_sw+':')
+    print()
+    print('interface GigabitEthernet4/'+sec_port)
+    print(' description '+loc.row+'-'+loc.rack+'-'+loc.slot+'-'+typeDict[type][2]+' '+alloc+' server - '+sid)
+    print(' switchport')
+    print(' switchport access vlan '+str(vlan))
+    if pri_or_sec == 'pri':
+        print(' switchport mode access')
+    else:
+        print(' private-vlan host-association 11 '+str(vlan))
+        print(' switchport mode private-vlan host')
+    print(' load-interval 30')
+    print(' speed '+speed)
+    print(' '+duplex)
+    print(' spanning-tree portfast edge')
+    print(' no shut')
+    print('end')
+    print()
 
 
 
